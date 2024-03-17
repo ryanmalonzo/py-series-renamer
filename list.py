0cb30fc07ccf7c5919752725dfdc2b555313e25f
Ryan Malonzo <64932650+yusa-ai@users.noreply.github.com>
2024-03-17 14:36:06 +0100

    Prompt user for info

--- list.py
+++
@@ -1,9 +0,0 @@
-import json
-
-if __name__ == "__main__":
-    with open("series.json") as series_json:
-        series = json.load(series_json)
-        for entry in series:
-            print(
-                f'{entry["name"]} - Season {entry["season"]} Episode {entry["episode"]}'
-            )
--- rename.py
+++ rename.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+
 import json
 from pathlib import Path
 from typing import List
 
 import click
+
+
+def get_json() -> None:
+    with open("series.json", "r") as file:
+        series: List = json.load(file)
+        return series
 
 
 def save_json(name, season, episode) -> None:
@@ -25,17 +33,23 @@ def save_json(name, season, episode) -> None:
 
 
 @click.command()
-@click.option(
-    "-n", "--name", type=str, required=True, help="The name of the series."
-)
-@click.option(
-    "-s", "--season", type=int, required=True, help="The season number."
-)
-@click.option(
-    "-e", "--episode", type=int, required=True, help="The episode number."
-)
 @click.argument("file_path", type=click.Path(exists=True))
-def rename(name, season, episode, file_path) -> None:
+def rename(file_path) -> None:
+    series_list: List = get_json()
+
+    # Print series' index and name
+    for index, entry in enumerate(series_list):
+        print(f'{index}. {entry["name"]}')
+
+    series_number: int = click.prompt("Series number", type=int)
+    series = series_list[series_number]
+
+    name: str = series["name"]
+    season = click.prompt("Season number", type=int, default=series["season"])
+    episode = click.prompt(
+        "Episode number", type=int, default=series["episode"] + 1
+    )
+
     file = Path(file_path)
     new_file_path = file.with_stem(f"{name} - S{season:02d}E{episode:02d}")
 
